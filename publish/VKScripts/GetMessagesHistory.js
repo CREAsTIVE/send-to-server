8c7796f87d2235a5ad964192953e54d30beb5b22
CREAsTIVE <alexshveev@gmail.com>
2023-04-09 22:35:25 +0500

    wrapper

---
+++ publish/VKScripts/GetMessagesHistory.js
@@ -0,0 +1,31 @@
+var replyId = $replyId$;
+var peerId = $peerId$;
+var counter = 0;
+var arr = [["user", "$$firstMessage$$"]];
+var groupId = $groupId$;
+var prefix = "$$prefix$$";
+var prefixLen = prefix.length;
+
+while (counter < 20 && replyId != null){
+	var msg = API.messages.getByConversationMessageId({
+		"peer_id": peerId, 
+		"conversation_message_ids": [replyId],
+		"extended": 0, 
+		"fields": ""
+	}).items[0];
+
+	replyId = msg.reply_message.conversation_message_id;
+
+	var from = "assistant";
+	if (msg.from_id != -groupId){
+		from = "user";
+	}
+	/*if (msg.text[0] == prefix) {
+		msg.text = msg.text.substr(prefixLen, msg.text.length);
+	}*/
+	arr.unshift([from, msg.text]);
+
+	counter = counter + 1; 
+}
+
+return arr;
---
+++ publish/VKScripts/GetMessagesHistoryNames.js
@@ -0,0 +1,39 @@
+var cmid = $cmid$;
+var replyId = $replyId$;
+var peerId = $peerId$;
+var counter = 0;
+
+var name = API.messages.getByConversationMessageId({
+	"peer_id": peerId,
+	"conversation_message_ids": [cmid],
+	"extended": 1,
+	"fields": ["first_name", "last_name"]
+}).profiles[0];
+
+var arr = [[name.first_name + " " + name.last_name, "$$firstMessage$$"]];
+var groupId = $groupId$;
+var prefix = "$$prefix$$";
+var prefixLen = prefix.length;
+
+while (counter < 20 && replyId != null){
+	var result = API.messages.getByConversationMessageId({
+		"peer_id": peerId, 
+		"conversation_message_ids": [replyId],
+		"extended": 0, 
+		"fields": ["first_name", "last_name"]
+	});
+	var msg = result.items[0];
+	var name = result.profiles[0].first_name + " " + result.profiles[0].last_name;
+
+	replyId = msg.reply_message.conversation_message_id;
+
+	var from = "assistant";
+	if (msg.from_id != -groupId){
+		from = name;
+	}
+	arr.unshift([from, msg.text]);
+
+	counter = counter + 1; 
+}
+
+return arr;
